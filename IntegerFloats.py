f54d6844bfdaf0f5d40909685c636ab3510ccfaf
Talibiq123 <talibsaeed90@gmail.com>
2023-07-04 15:51:21 +0530

    Integr & Float

---
+++ IntegerFloats.py
@@ -0,0 +1,49 @@
+# num = 7
+# print(type(num))
+
+# print(7 + 9)
+# print(9 - 7)
+# print(7 * 9)
+# print(7 / 9)
+# print(7 // 9)
+# print(9 % 7)
+# print(2 ** 3)
+
+# print(2 + 3 - 1 * 4 / 8)
+# print(2 - 1 + 3 * 2 / 2)
+
+# num = num + 1
+# num += 1
+# print(num)
+
+# abs()
+# num1 = -3
+# print(abs(num1))
+
+# print(round(abs(-2.322)))
+
+
+# num1 = 2
+# num2 = 3
+# print(num1 == num2)
+# print(num1 != num2)
+# print(num1 <= num2)
+# print(num1 >= num2)
+
+# Casting
+num1 = int('200')
+num2 = int('300')
+print(num1 + num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
