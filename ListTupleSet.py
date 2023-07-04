23faabbcd080ca29a6b684cdcc26238f39176b59
Talibiq123 <talibsaeed90@gmail.com>
2023-07-04 20:58:49 +0530

    List, Tuple & Set

---
+++ ListTupleSet.py
@@ -0,0 +1,75 @@
+# List
+# course = ['History', 'Math', 'Physics', 'CompSci']
+# print(course)
+
+# indexing on list
+# print(course[0])
+# print(course[1])
+# print(course[2])
+# print(course[3])
+
+# Negative Indexing
+# print(course[-1])
+# print(course[-2])
+# print(course[-3])
+# print(course[-4])
+
+# Slicing on List
+# print(course[0:])
+# print(course[:4])
+
+
+# append()
+# course.append('Art')
+# print(course)
+
+# course.insert(2, 'Bio')
+# print(course)
+
+# course.remove('Bio')
+# print(course)
+
+# course.sort()
+# course.reverse()
+# sorted_list = sorted(course)
+# print(sorted_list)
+
+# nums = [3, 1, 2, 5, 4]
+# print(min(nums))
+# print(max(nums))
+# print(sum(nums))
+# for course in course:
+#     print(course)
+
+# for index, course in enumerate(course):
+#     print(index, course)
+
+# list into string
+# my_courses = ' - '.join(course)
+# new_list = my_courses.split(' - ')
+# print(my_courses)
+# print(new_list)
+
+# strings = 'Talib'
+# print(help(str))
+
+# text = 'Hello_World'
+# print(text.split('_'))
+
+
+# Tuple
+# tuple_1 = ('History', 'Maths', 'Physics', 'CompSci')
+# print(tuple_1)
+
+
+
+# Sets
+set_1 = {'History', 'Maths', 'Physics', 'CompSci'}
+print(set_1)
+
+
+
+
+
+
+
