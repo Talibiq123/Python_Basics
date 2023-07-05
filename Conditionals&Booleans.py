b4bec54465a91581ae87289b83548e8ebcce3c43
Talibiq123 <talibsaeed90@gmail.com>
2023-07-05 16:26:56 +0530

    Conditionals

---
+++ Conditionals&Booleans.py
@@ -0,0 +1,52 @@
+# if True:
+#     print('Conditional was True.')
+
+# if False:
+#     print('Conditional was False.')
+
+# language = 'Python'
+# if language == 'Python':
+#     print('Conditional was True.')
+
+# language = 'Pyhton'
+# if language == 'Python':
+#     print('Language is Python.')
+# else:
+#     print('Not Match')
+
+
+# language = 'Java'
+# if language == 'Python':
+#     print('Language is Python.')
+# elif language == 'Java':
+#     print('Language is Java.')
+# elif language == 'JavaScript':
+#     print('Language is JavaScript.')
+# else:
+#     print('Not Match Found')
+
+
+
+# Boolean: and, or & not
+user = 'Admin'
+logged_in = 'True'
+if user == 'Admin' and logged_in:
+    print('Admin Page')
+else:
+    print('Bad Credantials')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
