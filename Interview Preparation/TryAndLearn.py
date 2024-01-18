e94b244dcb99ab868cb4fa73f07988439cb5ad78
Talibiq123 <talibsaeed90@gmail.com>
2024-01-18 17:25:40 +0530

    Python

---
+++ Interview Preparation/TryAndLearn.py
@@ -0,0 +1,105 @@
+# a = 3
+# b = 3
+#
+# # exponentiation
+# # print(a ** b)
+#
+#
+# # Example arithmetic expression
+# result = (4 + 3) * 2 / (5 - 1) ** 2
+#
+# # print(result)
+#
+# n1 = 14
+# n2 = 16
+# # print(n1/n2)
+#
+# # type
+# res = 0.875
+# # print(type(res))
+#
+# # Unpackaging
+# l1 = [1, 2, 3]
+# a, b, c = l1
+# # print(a, b, c)
+#
+#
+# # x = "Python "
+# # y = "is "
+# # z = "awesome"
+# # print(x + y + z)
+#
+# # def myfunc():
+# #   global x
+# #   x = "fantastic"
+# #
+# # myfunc()
+#
+# # print("Python is " + x)
+#
+#
+# # range()
+# x = range(6)
+# list1 = list(x)
+# print(list1)
+
+# comNum = 2+7j
+# num = int(comNum)
+# print(num)
+
+# import random
+#
+# print(random.randrange(1, 10))
+
+# a = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+# print(a)
+
+# for loop on Python
+# for i in 'banana':
+#     print(i, end=" ")
+
+# message = "Hello, World"
+# print(len(message))
+
+# txt = "The best things in life are free!"
+# print("free" in txt)
+
+# msg = "hello world"[6:]
+# print(msg)
+
+# mag = 'hello bijnor'
+# print(mag[:-1])
+
+a = "Hello, World!"
+print(a.upper())
+print(a)
+
+ans1 = a.replace('World!', 'Bijnor')
+print(ans1)
+
+asn2 = a.split(' ')
+print(asn2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
