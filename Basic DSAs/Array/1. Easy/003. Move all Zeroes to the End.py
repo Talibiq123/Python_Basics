98a120208b44039c8b686e5dd6c93d90d7b523d4
Talibiq123 <talibsaeed90@gmail.com>
2024-03-09 20:52:07 +0530

    Moves Zeroes

--- Basic DSAs/Array/1. Easy/003. Move all Zeroes to the End.py
+++ Basic DSAs/Array/1. Easy/003. Move all Zeroes to the End.py
@@ -1,13 +1,29 @@
+# My Method
+
+# def moves_zeroes(arr):
+#     res = []
+#     for num in arr:
+#         if num != 0:
+#             res.append(num)
+#
+#     while len(res) < len(arr):
+#         res.append(0)
+#
+#     return res
+#
+# nums = [1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0]
+# ans = moves_zeroes(nums)
+# print('Array After moving Zeroes -> ', ans)
+
+
+# Efficient Method
 def moves_zeroes(arr):
-    res = []
-    for num in arr:
-        if num != 0:
-            res.append(num)
-
-    while len(res) < len(arr):
-        res.append(0)
-
-    return res
+    j = 0
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[i], arr[j] = arr[j], arr[i]
+            j += 1
+    return arr
 
 nums = [1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0]
 ans = moves_zeroes(nums)
